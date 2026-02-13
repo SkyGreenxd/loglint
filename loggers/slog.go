e3edc4d62a90dc675c15c816bf7725a6ccf679d8
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2026-02-13 03:35:16 +0300

    add zap logger

---
+++ loggers/slog.go
@@ -0,0 +1,34 @@
+package loggers
+
+const (
+	SlogName = "slog"
+	SlogPath = "log/slog"
+)
+
+func init() {
+	Register(NewSlogLogger())
+}
+
+type SlogLogger struct {
+	BaseLogger
+}
+
+func NewSlogLogger() Logger {
+	// TODO: проверить мб чет не хватает
+	methods := []string{
+		"Debug",
+		"DebugContext",
+		"Info",
+		"InfoContext",
+		"Warn",
+		"WarnContext",
+		"Error",
+		"ErrorContext",
+		"Log",
+		"LogAttrs",
+	}
+
+	return &SlogLogger{
+		BaseLogger: NewBaseLogger(SlogName, SlogPath, methods),
+	}
+}
