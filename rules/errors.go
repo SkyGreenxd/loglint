0e0d1c60bd46b0f1fc3e7c3600951bc1bd26bceb
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2026-02-13 03:17:14 +0300

    add new errors

--- rules/errors.go
+++ rules/errors.go
@@ -3,9 +3,14 @@ package rules
 import "errors"
 
 var (
-	ErrRuleNotFound  = errors.New("rule not found")
-	ErrSensitiveData = errors.New("log message contains sensitive data (potential password, key or token)")
-	ErrNotEnglish    = errors.New("log message contains non-english characters")
-	ErrNotLowercase  = errors.New("log message must start with a lowercase letter")
-	ErrInvalidSymbol = errors.New("log message contains forbidden special characters or emojis")
+	ErrRuleNotFound    = errors.New("rule not found")
+	ErrSensitiveData   = errors.New("log message contains sensitive data (potential password, key or token)")
+	ErrNotEnglish      = errors.New("log message contains non-english characters")
+	ErrNotLowercase    = errors.New("log message must start with a lowercase letter")
+	ErrInvalidSymbol   = errors.New("log message contains forbidden special characters or emojis")
+	ErrRuleRegistered  = errors.New("rule already registered")
+	ErrDecoderCreation = errors.New("failed to create decoder")
+	ErrDecodeSettings  = errors.New("failed to decode settings")
+	ErrDecodeOptions   = errors.New("failed to decode rule options")
+	ErrInvalidRegex    = errors.New("invalid regular expression")
 )
